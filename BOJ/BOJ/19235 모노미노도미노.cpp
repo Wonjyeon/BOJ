7e2a47d74c5a86010d39e33dfcbb160e9073a8b9
Won-Juyeon <34434489+Wonjyeon@users.noreply.github.com>
2020-06-27 19:33:49 +0900

    Update 19235 모노미노도미노.cpp

--- BOJ/BOJ/19235 모노미노도미노.cpp
+++ BOJ/BOJ/19235 모노미노도미노.cpp
@@ -4,16 +4,15 @@
 #define endl '\n'
 using namespace std;
 
-typedef pair<int, int> pa;
 int N, t, x, y, grade = 0;
-pa blockArr[2][6][4];
+int blockArr[2][6][4];
 
 void checkSpecial() {
 	for (int c = 0; c < 2; c++) {
 		bool zeroFlag = false;
 		bool oneFlag = false;
 		for (int j = 0; j < 4; j++) {
-			if (blockArr[c][0][j].first != 0) {
+			if (blockArr[c][0][j] != 0) {
 				zeroFlag = true;
 				break;
 			}
@@ -21,32 +20,32 @@ void checkSpecial() {
 		if (zeroFlag == true) {
 			for (int i = 5; i >= 4; i--) {
 				for (int j = 0; j < 4; j++) {
-					blockArr[c][i][j] = { 0,0 };
+					blockArr[c][i][j] = 0;
 				}
 			}
 			for (int i = 3; i >= 0; i--) {
 				for (int j = 0; j < 4; j++) {
 					blockArr[c][i + 2][j] = blockArr[c][i][j];
-					blockArr[c][i][j] = { 0,0 };
+					blockArr[c][i][j] = 0;
 				}
 			}
 			continue;
 		}
 		for (int j = 0; j < 4; j++) {
-			if (blockArr[c][1][j].first != 0) {
+			if (blockArr[c][1][j] != 0) {
 				oneFlag = true;
 				break;
 			}
 		}
 		if (oneFlag == true) {
 			for (int j = 0; j < 4; j++) {
-				blockArr[c][5][j] = { 0,0 };
+				blockArr[c][5][j] = 0;
 			}
 
 			for (int i = 4; i >= 0; i--) {
 				for (int j = 0; j < 4; j++) {
 					blockArr[c][i + 1][j] = blockArr[c][i][j];
-					blockArr[c][i][j] = { 0,0 };
+					blockArr[c][i][j] = 0;
 				}
 			}
 		}
@@ -56,34 +55,31 @@ void checkSpecial() {
 void moveBlock(int c) {
 	bool stopFlag = false;
 	while (!stopFlag) {
-		int cnt = 0;		// ÀÌµ¿ÇÑ ºí·Ï ¼ö
+		int cnt = 0;		// ì´ë™í•œ ë¸”ë¡ ìˆ˜
 		for (int i = 4; i >= 0; i--) {
 			for (int j = 0; j < 4; j++) {
-				pa block = blockArr[c][i][j];
-				int type = block.first;
-				int bId = block.second;
-				if (type == 0)
-					continue;
+				int type = blockArr[c][i][j];
+				if (type == 0) continue;
 				if (type == 2) {
-					if (blockArr[c][i + 1][j].first == 0 && blockArr[c][i + 1][j + 1].first == 0) {
-						blockArr[c][i + 1][j] = block;
+					if (blockArr[c][i + 1][j] == 0 && blockArr[c][i + 1][j + 1] == 0) {
+						blockArr[c][i + 1][j] = type;
 						blockArr[c][i + 1][j + 1] = blockArr[c][i][j + 1];
-						blockArr[c][i][j] = { 0,0 };
-						blockArr[c][i][j + 1] = { 0,0 };
+						blockArr[c][i][j] = 0;
+						blockArr[c][i][j + 1] = 0;
 						cnt++;
 					}
 					j++;
 				}
 				else {
-					if (blockArr[c][i + 1][j].first == 0) {
-						blockArr[c][i][j] = { 0,0 };
-						blockArr[c][i + 1][j] = block;
+					if (blockArr[c][i + 1][j] == 0) {
+						blockArr[c][i][j] = 0;
+						blockArr[c][i + 1][j] = type;
 						cnt++;
 					}
 				}
 			}
 		}
-		if (cnt == 0)		// ÇÏ³ªµµ ÀÌµ¿ÇÏÁö ¾Ê¾Ò´Ù¸é
+		if (cnt == 0)		// í•˜ë‚˜ë„ ì´ë™í•˜ì§€ ì•Šì•˜ë‹¤ë©´
 			stopFlag = true;
 	}
 }
@@ -91,69 +87,53 @@ void moveBlock(int c) {
 void updateGrade() {
 	for (int c = 0; c < 2; c++) {
 		while (1) {
-			int sum = 0;			// »èÁ¦µÈ Çà or ¿­ÀÇ ¼ö
+			int sum = 0;			// ì‚­ì œëœ í–‰ or ì—´ì˜ ìˆ˜
 			for (int i = 5; i >= 0; i--) {
 				int cnt = 0;
 				for (int j = 0; j < 4; j++) {
-					if (blockArr[c][i][j].first == 0)
-						break;
+					if (blockArr[c][i][j] == 0) break;
 					cnt++;
 				}
 				if (cnt == 4) {
 					sum++;
-					for (int j = 0; j < 4; j++) {
-						blockArr[c][i][j].first = 0;
-						blockArr[c][i][j].second = 0;
-					}
+					for (int j = 0; j < 4; j++) 
+						blockArr[c][i][j] = 0;
 					grade++;
 				}
 			}
-			if (sum == 0)
-				break;
-			else
-				moveBlock(c);
+			if (sum == 0) break;
+			else moveBlock(c);
 		}
 
 	}
 	checkSpecial();
 }
 
-void insert(int id) {
+void insert() {
 	for (int c = 0; c < 2; c++) {
 		int tempX = 0;
-		if (c == 1)
+		if (c == 1) {
 			swap(x, y);
+			if (t == 2) t = 3;
+			else if (t == 3) t = 2;
+		}
 
 		if (t == 1) {
-			while (tempX < 6 && blockArr[c][tempX][y].first == 0)
+			while (tempX < 6 && blockArr[c][tempX][y] == 0)
 				tempX++;
-			tempX--;
-			blockArr[c][tempX][y].first = t;
-			blockArr[c][tempX][y].second = id;
+			blockArr[c][tempX - 1][y] = t;
 		}
-		if (c == 1 && t == 2) {
-			t = 3;
-		}
-		else if (c == 1 && t == 3) {
-			t = 2;
-		}
-		if (t == 2) {
-			while (tempX < 6 && blockArr[c][tempX][y].first == 0 && blockArr[c][tempX][y + 1].second == 0)
+		else if (t == 2) {
+			while (tempX < 6 && blockArr[c][tempX][y] == 0 && blockArr[c][tempX][y + 1] == 0)
 				tempX++;
-			tempX--;
-			blockArr[c][tempX][y].first = t;
-			blockArr[c][tempX][y].second = id;
-			blockArr[c][tempX][y + 1].first = t;
-			blockArr[c][tempX][y + 1].second = id;
+			blockArr[c][tempX - 1][y] = t;
+			blockArr[c][tempX - 1][y + 1] = t;
 		}
-		if (t == 3) {
-			while (tempX < 6 && blockArr[c][tempX][y].first == 0)
+		else if (t == 3) {
+			while (tempX < 6 && blockArr[c][tempX][y] == 0)
 				tempX++;
-			tempX--;
-			blockArr[c][tempX][y].first = t;
-			blockArr[c][tempX][y].second = id;
-			blockArr[c][tempX - 1][y].first = t;
-			blockArr[c][tempX - 1][y].second = id;
+			blockArr[c][tempX - 1][y] = t;
+			blockArr[c][tempX - 2][y] = t;
 		}
 	}
 	updateGrade();
@@ -164,7 +144,7 @@ int blockCount() {
 	for (int c = 0; c < 2; c++) {
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (blockArr[c][i][j].first != 0)
+				if (blockArr[c][i][j] != 0)
 					cnt++;
 			}
 		}
@@ -173,13 +153,12 @@ int blockCount() {
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	ios::sync_with_stdio(0);  cin.tie(0);
 
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> t >> x >> y;
-		insert(i);
+		insert();
 	}
 	cout << grade << endl;
 	cout << blockCount() << endl;
