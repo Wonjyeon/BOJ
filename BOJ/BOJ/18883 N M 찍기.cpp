230d0bba53d536b0d1cd41d9c535ee77f93721c3
wndus9382 <wndus9382@naver.com>
2020-07-06 14:48:17 +0900

    18883

---
+++ BOJ/BOJ/18883 N M 찍기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M, cnt = 0, num = 0;
+	cin >> N >> M;
+	num = N*M;
+	for (int i = 1; i <= num; i++) {
+		cout << i;
+		cnt++;
+		if (cnt == M) {
+			cout << '\n';
+			cnt = 0;
+		}
+		else cout << ' ';
+	}
+}
